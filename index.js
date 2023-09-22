53d341bab970e23859e2078d1f1b0677a62f6c79
farhanyp <farhan.yudha2016we@gmail.com>
2023-09-22 15:19:09 +0700

    setup express js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+const express = require("express")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const logger = require("morgan")
+const categoriesRouter = require("./app/api/v1/categories/router")
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Hello"
+    })
+})
+
+app.use('/api/v1/cms', categoriesRouter)
+
+app.listen(3000, () => {
+    console.log("Example app listening on port 3000")
+  })
